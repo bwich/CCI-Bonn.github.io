278333f29e4a5da43a1d18e50d0e3bf7b686b618
Jaeyoung Cho <joeyadamcho@gmail.com>
2024-11-29 14:42:39 +0100

    dev/prod support

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 HUGOxPARAMSxCMSxLOCAL_BACKEND=true \
 HUGO_MODULE_REPLACEMENTS="github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy/v5 -> ../../../modules/wowchemy,
@@ -7,5 +7,5 @@ github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-reveal -> ../..
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-seo -> ../../../modules/wowchemy-seo,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-core -> ../../../modules/wowchemy-core,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms -> ../../../modules/wowchemy-plugin-netlify-cms" \
-hugo server --panicOnWarning --renderStaticToDisk -F --port 80 --bind 0.0.0.0 \
+hugo --panicOnWarning --minify -F \
 --source "starters/$1" --destination "./dist"
